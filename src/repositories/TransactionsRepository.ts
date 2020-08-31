5e4ca4dd306f4442b86577e14d983fb0a58bd609
Guilherme Santos (Sidao92) <gui_psy92@hotmail.com>
2020-08-30 22:37:42 -0300

    [Challenge] - Done

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTranscationDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,34 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance: Balance = { income: 0, outcome: 0, total: 0 };
+
+    if (!this.transactions.length) {
+      return balance;
+    }
+
+    balance.income = this.transactions.reduce((income, transaction) => {
+      return income + (transaction.type === 'income' ? transaction.value : 0);
+    }, 0);
+
+    balance.outcome = this.transactions.reduce((income, transaction) => {
+      return income + (transaction.type === 'outcome' ? transaction.value : 0);
+    }, 0);
+
+    balance.total = balance.income - balance.outcome;
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTranscationDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
